e819e0c79d0ec9c374b0c6a1201b0975f1b54fbe
Anup Shrestha <anup.stha012@gmail.com>
2023-07-03 08:57:02 +0545

    fix: add pwa

---
+++ src/pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Head, Html, Main, NextScript } from "next/document"
+
+const Document = () => (
+  <Html>
+    <Head>
+      <link rel="manifest" href="/manifest.json" />
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+)
+export default Document
