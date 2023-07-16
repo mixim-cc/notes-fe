4d6209d099b11d3ad098b48aa5215af044cd6548
Anup Shrestha <anup.stha012@gmail.com>
2023-07-16 21:10:39 +0545

    feat: add public popover

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -2,13 +2,14 @@ import * as React from "react"
 import { cn } from "@/utils/cn"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
+import { Loader2 } from "lucide-react"
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background",
   {
     variants: {
       variant: {
-        default: "bg-base text-shade-primary hover:bg-base/90",
+        default: "bg-[#1d2228] text-white hover:bg-[#20262e]",
         outline: "border border-stroke-base hover:bg-base hover:text-shade-primary",
         ghost: "hover:bg-base-hover hover:text-shade-primary",
         link: "underline-offset-4 hover:underline text-primary",
@@ -32,11 +33,24 @@ export interface ButtonProps
   asChild?: boolean
   leftIcon?: React.ReactNode
   rightIcon?: React.ReactNode
+  isLoading?: boolean
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, leftIcon, rightIcon, children, ...props }, ref) => {
+  (
+    { className, variant, size, asChild = false, leftIcon, rightIcon, children, isLoading, ...props },
+    ref
+  ) => {
     const Comp = asChild ? Slot : "button"
+
+    if (isLoading) {
+      return (
+        <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props}>
+          <Loader2 className="animate-spin" />
+        </Comp>
+      )
+    }
+
     return (
       <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props}>
         {leftIcon && leftIcon} {children} {rightIcon && rightIcon}
