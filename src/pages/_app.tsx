3403a301be3bd68b39994217e379aa2464966fce
Anjil Bishowkarma <33276306+anzeal@users.noreply.github.com>
2023-07-17 12:46:45 +0545

    feat: whatsnew

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,14 @@ import Providers from "@/providers/provider"
 import { ThemeProvider } from "@/providers/theme-provider"
 import { triggerSync } from "@/services/redux/reducers/file-explorer-reducer"
 import { fontSans } from "@/utils/fonts"
+import { hasSeenWhatsNewModal } from "@/utils/userPreferences"
 import { ClerkProvider } from "@clerk/nextjs"
 import { Analytics } from "@vercel/analytics/react"
 import { HotkeysProvider } from "react-hotkeys-hook"
 import { useDispatch } from "react-redux"
 
 import { Toaster } from "@/components/ui/toaster"
+import WhatsNewModal from "@/components/global/WhatsNew"
 
 // export const metadata = {
 //   title: "Drafts",
@@ -29,6 +31,7 @@ export default function App({ Component, pageProps }) {
         <Providers>
           <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
             <Component {...pageProps} />
+            {!hasSeenWhatsNewModal() && <WhatsNewModal />}
             <Analytics />
             <Toaster />
           </ThemeProvider>
